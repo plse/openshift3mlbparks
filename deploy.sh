fac3fa0c317acc1a035c4a0d7b151d9b650b4c24
Your Name <you@example.com>
2015-09-15 14:13:12 +0200

    parameterized the attributes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
-NEW_PROJECT=$1
-OSE_MASTER=$2
-GIT_URL=$3
-TEMPLATE_JSON=$4
+OSE_MASTER=$1
+NEW_PROJECT=$2
+APP_NAME=$3
+APP_DOMAIN=$4
+GIT_URL=$5
 #ssh -l alice 192.168.122.100 "oc login -u alice --insecure-skip-tls-verify --server=https://ose-master.rhpaas.com:8443"
 #ssh -l alice 192.168.122.100 "oc new-project $NEW_PROJECT"
 #ssh -l alice 192.168.122.100 "oc project $NEW_PROJECT"
@@ -11,4 +12,4 @@ TEMPLATE_JSON=$4
 ssh -l alice 192.168.122.100 "oc login -u alice --insecure-skip-tls-verify --server=$OSE_MASTER"
 ssh -l alice 192.168.122.100 "oc new-project $NEW_PROJECT"
 ssh -l alice 192.168.122.100 "oc project $NEW_PROJECT"
-ssh -l alice 192.168.122.100 "oc process -f $TEMPLATE_JSON | oc create -f -"
+ssh -l alice 192.168.122.100 "oc process -f $TEMPLATE_JSON -v APPLICATION_NAME=$APP_NAME,APPLICATION_DOMAIN=$APP_DOMAIN,GIT_URL=$GIT_URL| oc create -f -"
