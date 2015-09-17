b80787a330124230324846bb5c41c83b0982db02
Your Name <you@example.com>
2015-09-17 16:04:32 +0200

    remove src and pom.xml

--- src/main/java/org/openshift/mlbparks/rest/JaxrsConfig.java
+++
@@ -1,9 +0,0 @@
-package org.openshift.mlbparks.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/ws")
-public class JaxrsConfig extends Application{
-
-}
